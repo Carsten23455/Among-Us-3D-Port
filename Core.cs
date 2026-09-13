6eab2bd7b4460e99f7eece5465cde70ddace1503
Carsten23455 <188935848+Carsten23455@users.noreply.github.com>
2026-09-13 02:18:43 -0500

    Add files via upload

---
+++ Core.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using AchievementsAPI;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using CorsacCosmetics;
+using CorsacCosmetics.Cosmetics;
+using HarmonyLib;
+using MiraAPI;
+using MiraAPI.PluginLoading;
+using Reactor;
+using Reactor.Networking;
+using Reactor.Networking.Attributes;
+using Reactor.Utilities;
+using xCloud;
+
+namespace AU3DPort
+{
+    [BepInPlugin("TeamMessHall.AmongUs2.5D", "Among Us 2.5D", "0.0.0")]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    [BepInDependency(MiraApiPlugin.Id)]
+    [BepInDependency(AchievementsAPIPlugin.Id)]
+    [BepInDependency(CorsacCosmeticsPlugin.Id)]
+    [BepInDependency(ModCompatibility.SubmergedId, BepInDependency.DependencyFlags.SoftDependency)]
+    [ReactorModFlags(ModFlags.RequireOnAllClients | ModFlags.DisableServerAuthority)]
+    public class Core : BasePlugin, IMiraPlugin
+    {
+
+        public Harmony Harmony { get; } = new("com.teammesshall.AmongUs3DPort");
+        public string OptionsTitleText => "AU3DPort";
+        public ConfigFile GetConfigFile() => Config;
+        public static bool IsMobile = OperatingSystem.IsAndroid() || OperatingSystem.IsIOS();
+
+        public override void Load()
+        {
+            ReactorCredits.Register("AU3DPort", "Dev-Production", true, ReactorCredits.AlwaysShow);
+            Harmony.PatchAll();
+            byte[] bundleData;
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AU3DPort.VanillaPort.Assets.Bundles.au3dhats.ccb"))
+            using (var ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                bundleData = ms.ToArray();
+            }
+
+            PluginCompat.AddBundleBytes(bundleData);
+        }
+
+    }
+}
